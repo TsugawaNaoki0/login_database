1704b0c5f0459ffea2dd31b347e88580849745da
TsugawaNaoki0 <104245092+TsugawaNaoki0@users.noreply.github.com>
2022-05-22 13:08:23 +0900

    Add files via upload

---
+++ encode_key_words.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+sentence = "+地震@揺れ@震度@マグニチュード*"
+print()
+print(sentence)
+print()
+sentence = sentence.encode()
+# sentence = sentence.decode()
+print("-----------------------------------------------------------------------")
+print()
+print(sentence)
+print()
+print("-----------------------------------------------------------------------")
